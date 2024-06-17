0c11636ce0858ea1f4327d12c10b561c14654da1
Jackson Wozniak <jjw30@comcast.net>
2024-06-16 22:25:44 -0400

    rename backend main class

--- backend/src/main/java/internal/api/backend/PersonalLibraryApplication.java
+++ backend/src/main/java/internal/api/backend/PersonalLibraryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class PersonalLibraryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(PersonalLibraryApplication.class, args);
     }
 
 }
