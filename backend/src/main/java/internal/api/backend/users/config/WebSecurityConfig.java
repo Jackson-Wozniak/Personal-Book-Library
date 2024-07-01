19afab87628ba394ec46f2558a0937a54dddc40f
Jackson Wozniak <jjw30@comcast.net>
2024-07-01 12:55:49 -0400

    reconfigure web security to remove startup warning (in logs) related to authentication provider

--- backend/src/main/java/internal/api/backend/users/config/WebSecurityConfig.java
+++ backend/src/main/java/internal/api/backend/users/config/WebSecurityConfig.java
@@ -5,13 +5,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -38,21 +35,9 @@ public class WebSecurityConfig {
         return http.build();
     }
 
-    protected void authenticationProvider(AuthenticationManagerBuilder managerBuilder){
-        managerBuilder.authenticationProvider(daoAuthenticationProvider());
-    }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder);
-        provider.setUserDetailsService(userService);
-        return provider;
     }
 
     public String[] getPermittedEndpoints(){
